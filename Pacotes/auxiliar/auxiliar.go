69562e9947d446647a1273551219d2a70294e225
JosuePLima <paulolima899@gmail.com>
2023-03-29 23:18:08 -0300

    Aula sobre pacotes

---
+++ Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,10 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+func Escrever() {
+
+	fmt.Println("Escrevendo do pacote auxiliar")
+}
---
+++ Pacotes/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+
+	fmt.Println("Escrevendo do arquivo main")
+	auxiliar.Escrever()
+
+}
